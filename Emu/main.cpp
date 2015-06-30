a91b6eec1422a3d741558b6d4d1a0446346be666
Sergio <sergiomanceranom@gmail.com>
2015-06-30 00:56:02 -0500

    Initial Commit

---
+++ Emu/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Emu
+//
+//  Created by Sergio Mancera on 30/06/15.
+//  Copyright (c) 2015 Sergio Mancera. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
