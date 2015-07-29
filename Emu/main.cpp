3f672d48273276403fa1813c54279e26d60dfc1d
Sergio <sergiomanceranom@gmail.com>
2015-07-28 20:43:00 -0500

    Peleando con SDL para poner text

--- Emu/main.cpp
+++ Emu/main.cpp
@@ -14,70 +14,162 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+SDL_Window* window = NULL;
+SDL_Renderer* renderer;
+TTF_Font* font;
+SDL_Texture* SurfaceToTexture( SDL_Surface* surf );
+SDL_Texture* solidTexture;
 
-bool init(){
-    bool exito = true;
-    //The window we'll be rendering to
-    SDL_Window* window = NULL;
-    
-    //The surface contained by the window
-    SDL_Surface* screenSurface = NULL;
-    
-    //Initialize SDL
-    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-    {
-        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
-        exito = false;
-    }
-    else{
-        window = SDL_CreateWindow( "PunBoy", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-        if( window == NULL )
-        {
-            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
-            exito = false;
-        }
-        else
-        {
-            //Get window surface
-            screenSurface = SDL_GetWindowSurface( window );
-    
-            
-            /*
-             Dibuja un pixel, lo guardo pa' luego
-             
-             Uint32 colour;
-            
-            colour = SDL_MapRGB( screenSurface->format, 50, 50, 50 );
-            
-            Uint32 *pixmem32;
-            
-            pixmem32 = (Uint32*) screenSurface->pixels;
-            
-            int y= 50;
-            int x= 50;
-            pixmem32[(y*screenSurface->w)+x] = colour;
-            */
-            
-        }
-    }
-    
-    return exito;
-}
+// Setup
+void Render();
+bool InitEverything();
+bool InitSDL();
+bool CreateWindow();
+bool CreateRenderer();
+void SetupRenderer();
+void CreateTextTextures();
 
+// Our new function for setting uo SDL_TTF
+bool SetupTTF( const std::string &fontName );
 
 int main(int argc, const char * argv[]) {
     
     std::string rom = argv[2];
 
-    init();
+    if(!InitEverything())
+        return -1;
     
     emu c_emu(rom);
     c_emu.init(false);
+            Render();
     for (;;) {
+
         if(!c_emu.emulateCycle()){
+            
             std::cout<<"Fin de la emulación";
             break;
         }
     }
     return 0;
 }
+
+void Render()
+{
+    // Clear the window and make it all red
+    SDL_RenderClear( renderer );
+    
+    SDL_Rect solidRect;
+    solidRect.x = 0;
+    solidRect.y = 0;
+    
+    // Render our text objects ( like normal )
+    SDL_RenderCopy( renderer, solidTexture, nullptr, &solidRect );
+    
+    // Render the changes above
+    SDL_RenderPresent( renderer);
+}
+
+
+bool InitEverything()
+{
+    if ( !InitSDL() )
+        return false;
+    
+    if ( !CreateWindow() )
+        return false;
+    
+    if ( !CreateRenderer() )
+        return false;
+    
+    SetupRenderer();
+    
+    if ( !SetupTTF( "/Library/Fonts/Arial.ttf" ) )
+        return false;
+    
+    CreateTextTextures();
+    
+    return true;
+}
+
+void CreateTextTextures(){
+    SDL_Surface* solid = TTF_RenderText_Solid( font, "solid", {255,255,255,0} );
+    solidTexture = SurfaceToTexture( solid );
+}
+
+SDL_Texture* SurfaceToTexture( SDL_Surface* surf )
+{
+    SDL_Texture* text;
+    
+    text = SDL_CreateTextureFromSurface( renderer, surf );
+    
+    SDL_FreeSurface( surf );
+    
+    return text;
+}
+
+bool InitSDL()
+{
+    if ( SDL_Init( SDL_INIT_EVERYTHING ) == -1 )
+    {
+        std::cout << " Failed to initialize SDL : " << SDL_GetError() << std::endl;
+        return false;
+    }
+    
+    return true;
+}
+
+bool SetupTTF( const std::string &fontName)
+{
+    // SDL2_TTF needs to be initialized just like SDL2
+    if ( TTF_Init() == -1 )
+    {
+        std::cout << " Failed to initialize TTF : " << TTF_GetError() << std::endl;
+        return false;
+    }
+    
+    // Load our fonts, with a huge size
+    font = TTF_OpenFont( fontName.c_str(), 90 );
+    
+    // Error check
+    if ( font == nullptr )
+    {
+        std::cout << " Failed to load font : " << TTF_GetError() << std::endl;
+        return false;
+    }
+    
+    return true;
+}
+
+bool CreateWindow()
+{
+    window = SDL_CreateWindow( "Server",0,0, SCREEN_WIDTH, SCREEN_HEIGHT, 0 );
+    
+    if ( window == nullptr )
+    {
+        std::cout << "Failed to create window : " << SDL_GetError();
+        return false;
+    }
+    
+    return true;
+}
+bool CreateRenderer()
+{
+    renderer = SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED );
+    
+    if ( renderer == nullptr )
+    {
+        std::cout << "Failed to create renderer : " << SDL_GetError();
+        return false;
+    }
+    
+    return true;
+}
+void SetupRenderer()
+{
+    // Set size of renderer to the same as window
+    SDL_RenderSetLogicalSize( renderer, SCREEN_WIDTH, SCREEN_HEIGHT );
+    
+    // Set color of renderer to red
+    SDL_SetRenderDrawColor( renderer, 0, 0, 0, 255 );
+}
+
